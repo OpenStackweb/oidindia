776a8f6567a472535acc920cabc67e2967d0c37d
Josh Lohse <yoshwaaa@gmail.com>
2022-09-27 10:37:41 -0400

    Updated with foundation content

--- src/templates/marketing-page.js
+++ src/templates/marketing-page.js
@@ -143,7 +143,7 @@ export const MarketingPageTemplate = class extends React.Component {
           </h2>
           <p>OpenInfra, Cloud Native, Magma & Hyperledger Days India 2022 is an open source infrastructure community event jointly hosted by OpenInfra, Cloud Native, Magma and Hyperledger communities.</p>
           <h3>
-            Magma
+            Magma Core Foundation
           </h3>
           <p>
             Magma began as a project to bring modern software defined networking techniques to bear on the challenges of rural Internet access. Our experience building rural networks taught us that policy-rich network edges and simple fabrics were a broadly applicable design approach for building flexible, low-cost, and scalable networks, even outside the data center. Magma’s early design decisions reflect this core insight as well as our earliest use cases: small-scale, low-cost community networks and for coverage extension through federation with existing mobile networks.
@@ -154,11 +154,11 @@ export const MarketingPageTemplate = class extends React.Component {
           <p>
             Modern apps rely on automated infrastructure, from massive data center clouds to the smallest edge nodes for IoT and 5G, and none of it is possible without open source software. This is Open Infrastructure: open source technologies enabling everyone to provide infrastructure for others to build solutions on. The OpenInfra Foundation and its 110,000 members from 187 countries exist to ensure each open source component is built and tested together, collaboratively, with a radical approach to openness we call the Four Opens: Open Source, Development, Design and Community.
           </p>
-          <h3>Cloud Native</h3>
+          <h3>Cloud Native Computing Foundation</h3>
           <p>
             Cloud native technologies empower organizations to build and run scalable applications in modern, dynamic environments such as public, private, and hybrid clouds. Containers, service meshes, microservices, immutable infrastructure, and declarative APIs exemplify this approach. These techniques enable loosely coupled systems that are resilient, manageable, and observable. Combined with robust automation, they allow engineers to make high-impact changes frequently and predictably with minimal toil. The Cloud Native Computing Foundation seeks to drive adoption of this paradigm by fostering and sustaining an ecosystem of open source, vendor-neutral projects. We democratize state-of-the-art patterns to make these innovations accessible for everyone.
           </p>
-          <h3>Hyperledger</h3>
+          <h3>Hyperledger Foundation</h3>
           <p>
             Hyperledger Foundation is an open source community focused on developing a suite of stable frameworks, tools and libraries for enterprise-grade blockchain deployments. It is a global collaboration, hosted by The Linux Foundation, and includes leaders in finance, banking, Internet of Things, supply chains, manufacturing and Technology. Built under technical governance and open collaboration, individual developers, service and solution providers, government associations, corporate members and end users are all invited to participate in the development and promotion of these game-changing technologies.
           </p>
